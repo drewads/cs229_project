436080ba732d20679904aac5d22a4947cfe79810
Drew Wadsworth <drew.wadsworth@gmail.com>
2021-05-03 20:35:43 -0700

    Sequential processing of images into matrix of x data and vector of y data

--- img_proc.py
+++ img_proc.py
@@ -1,27 +1,59 @@
-import imageio
+from PIL import Image
 import numpy as np
+import os
+from pathlib import Path
 
-# Creates list of lists where each element of the list is a row
-# and each element of a row is a pixel with 3 values
-def img_to_3D_list(img_path):
-    return imageio.imread(img_path)
+def resize(img):
+    return img.reduce(2, box=(76, 1, 524, 449)) if img.size == (600, 450) else img
 
-def img_to_3D_np_array(img_path):
-    return np.array(img_to_3D_list(img_path))
+def flatten_pixels(img_list):
+    return [color_val for pixel in img_list for color_val in pixel]
 
-# Row major version of 3D list. Format is
-# (elem[0, 0, 0], elem[0, 0, 1], elem[0, 0, 2], elem[0, 1, 0], ...)
-# where the notation here is elem[row, col, depth]
-def img_to_row_major_list(img_path):
-    img_3D_list = img_to_3D_list(img_path)
-    row_major_first_step = [pixel for row in img_3D_list for pixel in row]
-    return [color_val for pixel in row_major_first_step for color_val in pixel]
+def num_data(dir_path):
+    top_dir = Path(dir_path)
+    true_dir_path = top_dir / 'Melanoma'
+    false_dir_path = top_dir / 'NotMelanoma'
+    true_dir = os.listdir(true_dir_path)
+    false_dir = os.listdir(false_dir_path)
+    return len(true_dir) + len(false_dir)
 
-def img_to_flat_list(img_path):
-    return img_to_row_major_list(img_path)
+def slice_data_sequential(dir_path, batch_size):
+    top_dir = Path(dir_path)
+    true_dir_path = top_dir / 'Melanoma'
+    false_dir_path = top_dir / 'NotMelanoma'
+    true_dir = os.listdir(true_dir_path)
+    false_dir = os.listdir(false_dir_path)
+    true_iter = iter(true_dir)
+    false_iter = iter(false_dir)
+
+    true_counter = 0
+    false_counter = 0
+    batch_counter = 0
+    batch_x = []
+    batch_y = []
+
+    while (true_counter < len(true_dir) or false_counter < len(false_dir)):
+        if batch_counter == batch_size:
+            yield (np.array(batch_x), np.array(batch_y))
+            batch_counter = 0
+            batch_x = []
+            batch_y = []
+
+        if batch_counter < batch_size and true_counter < len(true_dir):
+            with Image.open(true_dir_path / next(true_iter)) as img:
+                sized_img = resize(img)
+                batch_x.append(flatten_pixels(list(sized_img.getdata())))
+                batch_y.append(1)
+            true_counter += 1
+            batch_counter += 1
+
+        if batch_counter < batch_size and false_counter < len(false_dir):
+            with Image.open(false_dir_path / next(false_iter)) as img:
+                sized_img = resize(img)
+                batch_x.append(flatten_pixels(list(sized_img.getdata())))
+                batch_y.append(0)
+            false_counter += 1
+            batch_counter += 1
     
-def img_to_row_major_np_array(img_path):
-    return np.array(img_to_row_major_list(img_path))
-
-def img_to_flat_np_array(img_path):
-    return img_to_row_major_np_array(img_path)
+    if len(batch_x) > 0:
+        yield (np.array(batch_x), np.array(batch_y))
