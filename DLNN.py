8f2e026fa21a9d7e591a193a2101761b1d258525
Drew Wadsworth <drew.wadsworth@gmail.com>
2021-05-24 19:06:11 -0700

    Partial update re: data fetching. Code currently broken

--- DLNN.py
+++ DLNN.py
@@ -7,11 +7,12 @@ from tensorflow.keras import regularizers
 # from keras.utils.vis_utils import plot_model
 import img_proc
 
-def DLNN(X,Y,nn_dims,epochs = 500,batch_size = 25,loss='binary_crossentropy',lambd = 1e-4):
+def DLNN(data,m,n,nn_dims,epochs = 500,batch_size = 25,loss='binary_crossentropy',lambd = 1e-4):
 	""" 
 	Inputs:
-		 X - training data (np.array(rows: #examples; cols: #features))
-		 Y - training data labels (np.array(rows: #examples; cols: 1))
+		 data - training data (np.array(rows: #examples; cols: #features), np.array(np.array(rows: #examples; cols: 1)))
+		 m - number of examples
+		 n - number of features
 		 nn_dims - list with number of neurons in each layer [#input layer, # 1st hidden layer, ..., 1 neuron with sigmoid]
 		 epochs - number of iterations to run gradient descent 
 		 batch_size - number of training examples to include in a batch
@@ -21,8 +22,6 @@ def DLNN(X,Y,nn_dims,epochs = 500,batch_size = 25,loss='binary_crossentropy',lam
 		trained_model - model trained in keras
 
 	"""
-	m = np.shape(X)[0] # number of examples
-	n = np.shape(X)[1] # number of features in an example 
 	print([m,n])
 	layers = len(nn_dims) # number of layers
 
@@ -38,7 +37,7 @@ def DLNN(X,Y,nn_dims,epochs = 500,batch_size = 25,loss='binary_crossentropy',lam
 	print(trained_model.summary())
 	# plot_model(trained_model, to_file='model_plot.png', show_shapes=True, show_layer_names=True)
 	trained_model.compile(loss = loss, optimizer = 'adam', metrics=['accuracy'])
-	trained_model.fit(X, Y, epochs = epochs, batch_size = batch_size)
+	trained_model.fit(data, epochs = epochs, batch_size = batch_size)
 
 	return trained_model
 
@@ -51,36 +50,39 @@ def normalize(X):
 	# return np.solve(Sigma,X_centred)
 
 def main():
-	X_train, y_train = next(img_proc.slice_data_sequential('data/train_sep', 1000))
-	model = DLNN(normalize(X_train),y_train,[1024,256,64,16,4,1],epochs = 20)
-	y_train_pred = model.predict(X_train)
+	# todo: allow normalization function to be passed to slice_data_sequential
+	train_data = img_proc.slice_data_sequential('data_proc/train_sep', 1000)
+	model = DLNN(train_data,img_proc.num_examples('data_proc/train_sep'),img_proc.num_features(),[1024,256,64,16,4,1],epochs = 20)
+	y_train_pred = model.predict(train_data)
 
-	X_valid, y_valid = next(img_proc.slice_data_sequential('data/valid', 1000))
-	y_test_pred = model.predict(normalize(X_test))
+	# todo: normalization function
+	valid_data = img_proc.slice_data_sequential('data/valid', 1000)
+	y_valid_pred = model.predict(valid_data)
     
 
-	auc_roc,threshold_best = evaluate.ROCandAUROC(y_valid_pred,y_valid,'ROC_valid_data_dlnn.jpeg')
+	# TODO: auc_roc,threshold_best = evaluate.ROCandAUROC(y_valid_pred,y_valid,'ROC_valid_data_dlnn.jpeg')
 
-	print(f"\nArea Under ROC = {auc_roc}")
-	tp,fn,fp,tn = evaluate.counts(y_train_pred, y_train, threshold = threshold_best)
-	acc,prec,sens,spec,F1 = evaluate.stats(tp,fn,fp,tn)
-	print("\nStats for predictions on train set:")
-	print(f"Threshold = {threshold_best}")
-	print(f"Accuracy = {acc}")
-	print(f"Precision = {prec}")
-	print(f"Sensitivity = {sens}")
-	print(f"Specificity = {spec}")
-	print(f"F1 score = {F1}")
+	# TODO:
+	# print(f"\nArea Under ROC = {auc_roc}")
+	# tp,fn,fp,tn = evaluate.counts(y_train_pred, y_train, threshold = threshold_best)
+	# acc,prec,sens,spec,F1 = evaluate.stats(tp,fn,fp,tn)
+	# print("\nStats for predictions on train set:")
+	# print(f"Threshold = {threshold_best}")
+	# print(f"Accuracy = {acc}")
+	# print(f"Precision = {prec}")
+	# print(f"Sensitivity = {sens}")
+	# print(f"Specificity = {spec}")
+	# print(f"F1 score = {F1}")
 
-	tp,fn,fp,tn = evaluate.counts(y_valid_pred, y_valid, threshold = threshold_best)
-	acc,prec,sens,spec,F1 = evaluate.stats(tp,fn,fp,tn)
-	print("\nStats for predictions on validation set:")
-	print(f"Threshold = {threshold_best}")
-	print(f"Accuracy = {acc}")
-	print(f"Precision = {prec}")
-	print(f"Sensitivity = {sens}")
-	print(f"Specificity = {spec}")
-	print(f"F1 score = {F1}")
+	# tp,fn,fp,tn = evaluate.counts(y_valid_pred, y_valid, threshold = threshold_best)
+	# acc,prec,sens,spec,F1 = evaluate.stats(tp,fn,fp,tn)
+	# print("\nStats for predictions on validation set:")
+	# print(f"Threshold = {threshold_best}")
+	# print(f"Accuracy = {acc}")
+	# print(f"Precision = {prec}")
+	# print(f"Sensitivity = {sens}")
+	# print(f"Specificity = {spec}")
+	# print(f"F1 score = {F1}")
 
 if __name__ == "__main__":
     main()
--- img_proc.py
+++ img_proc.py
@@ -1,6 +1,7 @@
 from PIL import Image
 import numpy as np
 import os
+import argparse
 from pathlib import Path
 
 def resize(img):
@@ -9,18 +10,29 @@ def resize(img):
 def flatten_pixels(img_list):
     return [color_val for pixel in img_list for color_val in pixel]
 
-def normalize_data(data):
-    return data #/ 255
+def num_features():
+    return 224 * 224 * 3
 
-def num_data(dir_path):
+def num_examples(dir_path):
+    return num_examples_class(dir_path, 'Melanoma') + num_examples_class(dir_path, 'NotMelanoma')
+
+def num_examples_class(dir_path, classname):
     top_dir = Path(dir_path)
-    true_dir_path = top_dir / 'Melanoma'
-    false_dir_path = top_dir / 'NotMelanoma'
-    true_dir = os.listdir(true_dir_path)
-    false_dir = os.listdir(false_dir_path)
-    return len(true_dir) + len(false_dir)
+    class_dir_path = top_dir / classname
+    class_dir = os.listdir(class_dir_path)
+    return len(class_dir)
 
 def slice_data_sequential(dir_path, batch_size):
+    """
+    slice_data_sequential returns a generator that allows iteration through the
+    melanoma dataset at dir_path
+    Args:
+        dir_path: directory containing train, validate, or test data. Must have
+            subdirectories named 'Melanoma' and 'NotMelanoma'
+        batch_size: size of batch returned on each iteration of the generator
+    Returns:
+        generator that itself returns (x_data, y_data) on each iteration
+    """
     top_dir = Path(dir_path)
     true_dir_path = top_dir / 'Melanoma'
     false_dir_path = top_dir / 'NotMelanoma'
@@ -37,26 +49,65 @@ def slice_data_sequential(dir_path, batch_size):
 
     while (true_counter < len(true_dir) or false_counter < len(false_dir)):
         if batch_counter == batch_size:
-            yield (normalize_data(np.array(batch_x)), np.array(batch_y))
+            yield (np.array(batch_x), np.array(batch_y))
             batch_counter = 0
             batch_x = []
             batch_y = []
 
         if batch_counter < batch_size and true_counter < len(true_dir):
             with Image.open(true_dir_path / next(true_iter)) as img:
-                sized_img = resize(img)
-                batch_x.append(flatten_pixels(list(sized_img.getdata())))
+                batch_x.append(flatten_pixels(list(img.getdata())))
                 batch_y.append(1)
             true_counter += 1
             batch_counter += 1
 
         if batch_counter < batch_size and false_counter < len(false_dir):
             with Image.open(false_dir_path / next(false_iter)) as img:
-                sized_img = resize(img)
-                batch_x.append(flatten_pixels(list(sized_img.getdata())))
+                batch_x.append(flatten_pixels(list(img.getdata())))
                 batch_y.append(0)
             false_counter += 1
             batch_counter += 1
     
     if len(batch_x) > 0:
-        yield (normalize_data(np.array(batch_x)), np.array(batch_y))
+        yield (np.array(batch_x), np.array(batch_y))
+
+# gets data from dir_path_src, resizes it to 224x224, and saves it in dir_path_dest
+def process_data(dir_path_src, dir_path_dest):
+    """
+    gets data from dir_path_src, resizes it to 224x224, and saves it in dir_path_dest
+    Args:
+        dir_path_src: directory containing train, validate, or test data. Must have
+            subdirectories named 'Melanoma' and 'NotMelanoma'
+        dir_path_dest: directory to contain resized train, validate, or test data.
+            Must have subdirectories named 'Melanoma' and 'NotMelanoma'
+    """
+    top_dir_src = Path(dir_path_src)
+    true_dir_path_src = top_dir_src / 'Melanoma'
+    false_dir_path_src = top_dir_src / 'NotMelanoma'
+    true_iter = iter(os.listdir(true_dir_path_src))
+    false_iter = iter(os.listdir(false_dir_path_src))
+
+    top_dir_dest = Path(dir_path_dest)
+    true_dir_path_dest = top_dir_dest / 'Melanoma'
+    false_dir_path_dest = top_dir_dest / 'NotMelanoma'
+
+    for src_img_name in true_iter:
+        with Image.open(true_dir_path_src / src_img_name) as src_img:
+            sized_img = resize(src_img)
+            sized_img.save(true_dir_path_dest / src_img_name)
+
+    for src_img_name in false_iter:
+        with Image.open(false_dir_path_src / src_img_name) as src_img:
+            sized_img = resize(src_img)
+            sized_img.save(false_dir_path_dest / src_img_name)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Resize image data to 224x224')
+    parser.add_argument('data_dir')
+    args = parser.parse_args()
+
+    data_dir = Path(args.data_dir)
+
+    process_data(data_dir / 'train_sep', data_dir / 'train_sep')
+    process_data(data_dir / 'valid', data_dir / 'valid')
+    process_data(data_dir / 'test', data_dir / 'test')
