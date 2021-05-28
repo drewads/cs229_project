227c9f5b061814d74c2113c4c865a6c2583c06a2
Sophie Parsa <sjparsa@stanford.edu>
2021-05-27 19:52:40 -0700

    Merge branch 'main' of /Users/sjparsa/Documents/cs229_project with conflicts.

---
+++ transfer.py
@@ -0,0 +1,29 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+
+
+def transfer():
+    base_model = keras.applications.ResNet50(
+        weights='imagenet',
+        include_top=False)
+
+
+
+
+
+
+
+
+def main():
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
+
+
