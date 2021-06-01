e2916705f7ffdf1445c5363c277511a4ffb144ca
Sophie Parsa <sjparsa@stanford.edu>
2021-05-31 23:35:04 -0700

    try without multi

--- feature_analysis.py
+++ feature_analysis.py
@@ -1,4 +1,13 @@
 import numpy as np
+from PIL import Image
+
+def grayscale(vector):
+    min_val = min(vector)
+    max_val = max(vector)
+    grayscale_vector = np.empty(len(vector))
+    for i in range(len(vector)):
+        grayscale_vector[i] = (vector[i]-min_val)/(max_val-min_val)
+    return grayscale_vector.reshape((224,224))
 
 def main():
     weights = np.genfromtxt('log_reg_weights.csv', delimiter=",")
@@ -8,6 +17,29 @@ def main():
     np.savetxt("red_log_reg_weights.csv", red_weights, delimiter=",")
     np.savetxt("green_log_reg_weights.csv", green_weights, delimiter=",")
     np.savetxt("blue_log_reg_weights.csv", blue_weights, delimiter=",")
+
+    grayscale_red = grayscale(red_weights)
+    grayscale_green = grayscale(green_weights)
+    grayscale_blue = grayscale(blue_weights)
+    # print(grayscale_red[0])
+    # tmp = np.empty(224)
+    # for i in range(224):
+    #     tmp[i] = (red_weights[i]-min(red_weights))/(max(red_weights)-min(red_weights))
+    # print(tmp[0:224])
+
+    # Creates PIL image
+    img = Image.fromarray(np.uint8(grayscale_red * 255), 'L')
+    img.show()
+
+    # Creates PIL image
+    img = Image.fromarray(np.uint8(grayscale_green * 255), 'L')
+    img.show()
+
+    # Creates PIL image
+    img = Image.fromarray(np.uint8(grayscale_blue * 255), 'L')
+    img.show()
+
+
 
 if __name__ == '__main__':
     main()
