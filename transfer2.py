825ad0062eaa358c5b883d0b2c406f98155d05b6
tcwelch <43589658+tcwelch@users.noreply.github.com>
2021-06-01 21:20:25 -0700

    new transfer with cnn

--- transfer2.py
+++ transfer2.py
@@ -15,6 +15,24 @@ def transfer_learning(data_gen, base_model, epochs=20):
     X = base_model(X_input)
     
     # start making layers here
+    X = ZeroPadding2D((3, 3))(X) #shape: 13x13x2048
+    X = Conv2D(filters=2048, kernel_size=(7, 7), strides = (1, 1), name = 'conv0')(X) #shape: 7x7x2048
+    X = BatchNormalization(axis = 3, name = 'bn0')(X)
+    X = Activation('relu')(X)
+    X = MaxPooling2D(pool_size=(3, 3), strides=(2,2),name='max_pool0')(X) #shape: 3x3x2048
+
+    X = ZeroPadding2D((1, 1))(X) #shape: 5x5x2048
+    X = Conv2D(filters=4096, kernel_size=(3, 3), strides = (1, 1), name = 'conv1')(X) #shape: 3x3x4096
+    X = BatchNormalization(axis = 3, name = 'bn1')(X)
+    X = Activation('relu')(X)
+    X = MaxPooling2D(pool_size=(3, 3), strides=(2,2),name='max_pool1')(X) #shape: 2x2x4096
+
+    X = Flatten()(X) #shape: 16384
+    X = Dense(units=4096, activation='relu', name='fc0')(X)
+    X = Dense(units=1024, activation='relu', name='fc1')(X)
+    X = Dense(units=256, activation='relu', name='fc2')(X)
+    X = Dense(units=32, activation='relu', name='fc3')(X)
+    X = Dense(units=1, activation='sigmoid', name='fc4')(X)
 
     model = Model(inputs = X_input, outputs = X, name='CNN') # Total number of trainable params = 737,537
     model.compile(optimizer = "Adam", loss = 'binary_crossentropy', metrics = ["accuracy"])
@@ -32,7 +50,7 @@ def main(data_dir,BATCH_SIZE=100):
     feats = img_proc.Data_Generator(data_dir / 'train_sep', BATCH_SIZE, shuffle=True, flatten=False)
 
     print('---------- Training Model ----------')
-    model = transfer_learning(feats, base_model, epochs=20)
+    model = transfer_learning(feats, base_model, epochs=1)
 
     print('---------- Saving Model ----------')
     model.save('saved_transfer_model')
